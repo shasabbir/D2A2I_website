6450386c9089ccf37bc2d3f3b874984ecc553845
shasabbir <shasabbir234@gmail.com>
2023-01-26 01:45:24 +0600

    dashboard fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
+import Dashboard from './Component/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+      <Dashboard/>
 
     </div>
   );
